b45d721754c64197dad08433b46f6e7efc6e6bbd
MinhVod <voduyminh1991@gmail.com>
2021-09-26 00:19:39 +0700

    1st training
    open google

---
+++ Test Suites/MV_TS1.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>MV_TS1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>8d56c361-2aae-429a-9e65-0e9012d2365d</testSuiteGuid>
+   <testCaseLink>
+      <guid>d132fedc-aa58-49f6-ac90-f39bc8ab5142</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/MV_TC1</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b4dacf07-c228-4b84-beae-810cbc0fd612</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
